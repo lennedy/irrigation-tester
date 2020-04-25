24baff5e4512d92575469d59648a4bda196e82bc
lennedy <lennedy@gmail.com>
2020-04-24 21:04:44 -0300

    - Diversas modificações para permitir utilização dos arquivos ComunicationActuator.py e Comunication.py. - Criação de frame setButton que engloba os botões e também um RadioButton. - Criação de função updateFromServer para realizar a atualizacao dos dados que estiverem no servidor.

--- gui.py
+++ gui.py
@@ -1,155 +1,228 @@
-import Tkinter as tk
-from Tkinter import *
+import tkinter as tk
+from tkinter import *
 from PIL import ImageTk, Image
+from ComunicationActuator import *
 import os
 import logging
 
 class PhotoFrame(tk.Frame):
     
-    def __init__(self, master, img1, img2, name="my_frame"):
+	def __init__(self, master, img1, img2, name="my_frame"):
 
-        tk.Frame.__init__(self, master, relief='ridge', bd=2)
-        self.updated = False
-        self.img1_file= img1
-        self.img2_file= img2
-#        self.button_widget()
-        self.loadImage()
-        self.open = False
+		tk.Frame.__init__(self, master, relief='ridge', bd=2)
+		self.updated = False
+		self.updatedByGui = False
+		self.img1_file= img1
+		self.img2_file= img2
+		#        self.button_widget()
+		self.loadImage()
+		self.open = False
 
+	def label_widget(self, s):
+		self.title_label = tk.Label(self,text=s)  # Or Frame 1, 2 etc.
+		self.title_label.grid(row=0, column=0, columnspan=1, pady=5)
 
-    def label_widget(self, s):
-        self.title_label = tk.Label(self,text=s)  # Or Frame 1, 2 etc.
-        self.title_label.grid(row=0, column=0, columnspan=1, pady=5)
+	def loadImage(self, open=False):
+		if(open):
+			self.img = tk.Label(self, image = self.img1_file)
+			self.open = True
+		else:
+			self.img = tk.Label(self, image = self.img2_file)
+			self.open = False
+		self.updated = True
+#		print(self.updated)
+		self.img.grid(row=3, column=2, columnspan=4, pady=5)
+		self.img.bind('<Double-1>', self.double_click)
 
-    def loadImage(self, open=False):
-        if open:
-          self.img = tk.Label(self, image = self.img1_file)
-          self.open = True
-        else:
-          self.img = tk.Label(self, image = self.img2_file)
-          self.open = False
-        self.updated = True
-        print(self.updated)
-        self.img.grid(row=3, column=2, columnspan=4, pady=5)
-        self.img.bind('<Double-1>', self.double_click)
+	def button_widget(self):
+		self.button = tk.Button(self,text="Open", command = self.updateOpen)
+		self.button.grid(row=0, column=2, columnspan=4)
+		self.button = tk.Button(self,text="Close", command = self.updateClose)
+		self.button.grid(row=2, column=2, columnspan=4)
+	
+	def pressedButton(self, open=False):
+		self.updatedByGui=True
+		self.loadImage(open)
 
-    def button_widget(self):
-        self.button = tk.Button(self,text="Open", command = self.updateOpen)
-	self.button.grid(row=0, column=2, columnspan=4)
-        self.button = tk.Button(self,text="Close", command = self.updateClose)
-	self.button.grid(row=2, column=2, columnspan=4)
-
-    def double_click(self, event):
-        if self.open:
-          self.loadImage(False)
-        else:
-          self.loadImage(True)
+	def double_click(self, event):
+		self.updatedByGui=True
+		if self.open:
+			self.loadImage(False)
+		else:
+			self.loadImage(True)
  
 
-    def updateOpen(self):
-      self.loadImage(True)
+	def updateOpen(self):
+		self.loadImage(True)
 
-    def updateClose(self):
-      self.loadImage(False)
+	def updateClose(self):
+		self.loadImage(False)
 
-    def itWasUpdated(self):
-	updated=False
-	
-	if (self.updated==True):
-	  updated=True	  
-	  
-	self.updated=False
-	
-	return updated
+	def wasItUpdated(self):
+		updated=False
+		updatedByGui = False
+
+		if (self.updated == True):
+			updated=True
+		if (self.updatedByGui == True):
+			updatedByGui=True
+		  
+		self.updated=False
+		self.updatedByGui = False
+
+		return updatedByGui
+    
+	def isActive(self):
+		return self.open 
+
+	def changeValveState(self, open=False):
+		self.loadImage(open)
+
+class RadioFrame(tk.Frame):
+	def __init__(self, master, name="my_frame"):
+		tk.Frame.__init__(self, master, relief='ridge', bd=2)
+
+		self.automatic = IntVar()
+		self.r1 = Radiobutton(self, text="Automático", variable=self.automatic, value=1, command=self.changed)
+		self.r2 = Radiobutton(self, text="Manutal", variable=self.automatic, value=0, command=self.changed)
+		self.r1.grid(row=1, column=1, columnspan=10)
+		self.r2.grid(row=2, column=1, columnspan=1)
+		self.itWasUpdated=False
+
+	def isItAutomatic(self):
+		return ( self.automatic.get()==1 )
+
+	def changed(self):
+		self.itWasUpdated=True
+
+	def wasItUpdated(self):
+		itWasUpdated = self.itWasUpdated
+		self.itWasUpdated =  False
+		return itWasUpdated
+
+	def changeState(self, state=False):
+		self.automatic.set(IntVar(state))
 
 class ButtonFrame(tk.Frame):
 
-    def __init__(self, master, c, name="button_frame"):
+	def __init__(self, master, c, name="button_frame"):
 
-        tk.Frame.__init__(self, master, relief='ridge', bd=2)
-        self.label_widget(name)
-	self.command=c
+		tk.Frame.__init__(self, master, relief='ridge', bd=2)
+		self.label_widget(name)
+		self.command=c
 
-        self.button = tk.Button(self,text="Open", command = self.updateOpen)
-	self.button.grid(row=2, column=2, columnspan=4)
-        self.button = tk.Button(self,text="Close", command = self.updateClose)
-	self.button.grid(row=4, column=2, columnspan=4)
+		self.button = tk.Button(self,text="Open", command = self.updateOpen)
+		self.button.grid(row=2, column=2, columnspan=4)
+		self.button = tk.Button(self,text="Close", command = self.updateClose)
+		self.button.grid(row=4, column=2, columnspan=4)
 
-    def label_widget(self, s):
-        self.title_label = tk.Label(self,text=s)  # Or Frame 1, 2 etc.
-        self.title_label.grid(row=0, column=2, columnspan=4, pady=5)
+	def label_widget(self, s):
+		self.title_label = tk.Label(self,text=s)  # Or Frame 1, 2 etc.
+		self.title_label.grid(row=0, column=2, columnspan=4, pady=5)
 
-    def updateOpen(self):
-      self.command(True)
+	def updateOpen(self):
+		self.command(True)
 
-    def updateClose(self):
-      self.command(False)
+	def updateClose(self):
+		self.command(False)
 
 class SetButtonFrame(tk.Frame):
 
-    def __init__(self, master, c1, c2, c3, name="set_buttons"):
-        tk.Frame.__init__(self, master, relief='ridge', bd=2)
-        self.commad1=c1
-        self.commad2=c2
-        self.commad3=c3
+	def __init__(self, master, c1, c2, c3, name="set_buttons"):
+		tk.Frame.__init__(self, master, relief='ridge', bd=2)
+		self.commad1=c1
+		self.commad2=c2
+		self.commad3=c3
 
-        self.buttonFrame1 = ButtonFrame(self, c1, name="Valv1")
-	self.buttonFrame1.grid(row=0, column=2, columnspan=1)
-        self.buttonFrame2 = ButtonFrame(self, c2, name="Valv2")
-	self.buttonFrame2.grid(row=0, column=8, columnspan=1)
-        self.buttonFrame3 = ButtonFrame(self, c3, name="Valv3")
-	self.buttonFrame3.grid(row=0, column=12, columnspan=1)
-        self.buttonFrame4 = ButtonFrame(self, self.changeAllStates, name="All Valv")
-	self.buttonFrame4.grid(row=0, column=0, columnspan=1)
+		self.buttonFrame1 = ButtonFrame(self, c1, name="Valv1")
+		self.buttonFrame1.grid(row=0, column=2, columnspan=1)
+		self.buttonFrame2 = ButtonFrame(self, c2, name="Valv2")
+		self.buttonFrame2.grid(row=0, column=8, columnspan=1)
+		self.buttonFrame3 = ButtonFrame(self, c3, name="Valv3")
+		self.buttonFrame3.grid(row=0, column=12, columnspan=1)
+		self.buttonFrame4 = ButtonFrame(self, self.changeAllStates, name="All Valv")
+		self.buttonFrame4.grid(row=0, column=0, columnspan=1)
+
+		self.radioFrame = RadioFrame(self, name)
+		self.radioFrame.grid(row=0, column=14)
 
 
-    def changeAllStates(self, state):
-        self.commad1(state)
-        self.commad2(state)
-        self.commad3(state)
 
-class Comunication():
+	def changeAllStates(self, state):
+		self.commad1(state)
+		self.commad2(state)
+		self.commad3(state)
 
-    def setValvPumpValues(self):
-	print ("Send Data")
-#	pl = {"pumps": pump_val,
-#	    "valves": valve_val}
-#	send_pump_valve_value(pl)
+	def isItAutomatic(self):
+		return self.radioFrame.isItAutomatic()
+
+	def wasItUpdated(self):
+		return self.radioFrame.wasItUpdated()
+
+	def changeRadioState(self, state):
+		self.radioFrame.changeState(state);
 
 
 class App():
 
-    def __init__(self):
-      self.comunication = Comunication()
-      self.root = Tk()
-      self.root.title("Simulador de Irrigacao")
-      image_size=70
-      self.img1 = ImageTk.PhotoImage(Image.open("valv_abert.gif").resize((image_size+30, image_size)))
-      self.img2 = ImageTk.PhotoImage(Image.open("valv_fechada.gif").resize((image_size+30, image_size)))
+	def __init__(self):
+		self.comunication = ComunicationActuator()
+		self.root = Tk()
+		self.root.title("Simulador de Irrigacao")
+		image_size=70
+		self.img1 = ImageTk.PhotoImage(Image.open("valv_abert.gif").resize((image_size+30, image_size)))
+		self.img2 = ImageTk.PhotoImage(Image.open("valv_fechada.gif").resize((image_size+30, image_size)))
 
-      self.f1 = PhotoFrame(self.root, self.img1, self.img2, "f1")
-      self.f1.pack( side = TOP )
+		self.f1 = PhotoFrame(self.root, self.img1, self.img2, "f1")
+		self.f1.pack( side = TOP )
 
-      self.f2 = PhotoFrame(self.root, self.img1, self.img2, "f2")
-      self.f2.pack( side = TOP )
+		self.f2 = PhotoFrame(self.root, self.img1, self.img2, "f2")
+		self.f2.pack( side = TOP )
 
-      self.f3 = PhotoFrame(self.root, self.img1, self.img2, "f3")
-      self.f3.pack( side = TOP )
+		self.f3 = PhotoFrame(self.root, self.img1, self.img2, "f3")
+		self.f3.pack( side = TOP )
 
-      self.setButton = SetButtonFrame(self.root, self.f1.loadImage, self.f2.loadImage, self.f3.loadImage)
-      self.setButton.pack( side = BOTTOM )
+		self.setButton = SetButtonFrame(self.root, self.f1.pressedButton, self.f2.pressedButton, self.f3.pressedButton)
+		self.setButton.pack( side = BOTTOM )
 
-    def updateFromServer(self):
+		self.comunication.addValv( Valve(pin = -1, name =  "valve1") )
+		self.comunication.addValv( Valve(pin = -1, name =  "valve2") )
+		self.comunication.addValv( Valve(pin = -1, name =  "valve3") )
 
-	if(self.f1.itWasUpdated()):
-	  self.comunication.setValvPumpValues()
-	self.root.after(1000, self.updateFromServer) 
+	def updateFromServer(self):
+		automatic =  self.setButton.isItAutomatic()
+		aFrameWasUpdated=False
+		if( self.f1.wasItUpdated() ):
+			aFrameWasUpdated=True
+		if( self.f2.wasItUpdated() ):
+			aFrameWasUpdated=True
+		if( self.f3.wasItUpdated() ):
+			aFrameWasUpdated=True
+		if( self.setButton.wasItUpdated() ):
+			aFrameWasUpdated=True
 
 
-    def main(self):
-	self.updateFromServer()
-	self.root.mainloop()
+		self.comunication["valve1"] = self.f1.isActive()
+		self.comunication["valve2"] = self.f2.isActive()
+		self.comunication["valve3"] = self.f3.isActive()
+		self.comunication.updateAtomatic(automatic)
+		print("Automatico: ",automatic)
+		if( aFrameWasUpdated ):
+			self.comunication.updateActuatorsInServer()
 
+		self.comunication.getDataInServer()
+		##self.comunication.update()
+
+		self.f1.changeValveState(self.comunication["valve1"])
+		self.f2.changeValveState(self.comunication["valve2"])
+		self.f3.changeValveState(self.comunication["valve3"])
+
+		self.root.after(1000, self.updateFromServer) 
+
+
+	def main(self):
+		self.updateFromServer()
+		self.root.mainloop()
 
 
 app = App()
